87849a044d9671dfe0bf1631f326382773f9c4ca
Julian <olvidao.net@me.com>
2014-01-29 16:38:31 +0100

    Version 0.0.1

---
+++ com/Julian/Client/GUI/Components/DirectoryPanel.java
@@ -0,0 +1,23 @@
+
+
+package com.Julian.Client.GUI.Components;
+
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+/**
+ *
+ * @author julialonso
+ */
+public class DirectoryPanel extends JPanel {
+    
+    private JList dir;
+    private JScrollPane dirPane;
+    
+    public DirectoryPanel() {
+        
+    }
+    
+    
+}
---
+++ com/Julian/Client/GUI/GUIClient.java
@@ -0,0 +1,13 @@
+
+
+package com.Julian.Client.GUI;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author julialonso
+ */
+public class GUIClient extends JPanel {
+    
+}
